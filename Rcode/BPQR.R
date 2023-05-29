cf7379442d2f5c3ef33c8dd9b569e4a716c56eab
Eva Murphy <103435906+evamurphy100@users.noreply.github.com>
2023-05-29 17:39:17 -0400

    Update BPQR.R

--- Rcode/BPQR.R
+++ Rcode/BPQR.R
@@ -10,16 +10,14 @@ BPQR = function(WS, WD, df, p = c(0.5, 0.95){
   # WD - wind direction data in radians
   # df = degrees of freedom to use with pbs()
   # p = quantile level
-# Define the knots and degree for periodic b-spline fitting fitting
-inner_knots<- seq.int(0+10*pi/180, 2*pi-10*pi/180, length.out = 35)
+# Define the starting and ending knots for periodic b-spline fitting fitting
 boundary_knots<- c(0, 2*pi)
-degree<-3
 # Define the direction values for prediction
 xg.qr = seq(0, 2*pi, 0.01)
 # Organize wind direction and wind speed in a data frame
 Data.win<- data.frame(WD.qr = WD , WS.qr = WS)
 # Fit a quantile regressions with periodic b-splines
-QRfit<- rq(WS.qr ~ pbs(WD.qr, knots = inner_knots, degree = degree , Boundary.knots = boundary_knots), tau = p, data = Data.win)
+QRfit<- rq(WS.qr ~ pbs(WD.qr, df = df, Boundary.knots = boundary_knots), tau = p, data = Data.win)
 # Use the fitted model to make prediction
 QRpred<- predict(QRfit.win, data.frame(WD.qr = xg.qr))
 }
